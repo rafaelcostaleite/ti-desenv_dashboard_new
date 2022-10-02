c311054d86799aa1458db9dbe7e77cd2f3d36db3
Rafal Leite <rafaelcostaleite@gmail.com>
2022-10-01 23:17:53 -0300

    ajusando docker compose

--- mysql/tables.sql
+++ mysql/tables.sql
@@ -6,6 +6,10 @@ CREATE TABLE IF NOT EXISTS incidentes_historico (
     incidente_sem_classificacao INT,
     incidente_sem_alocacao INT);
 
+SET default-authentication-plugin=mysql_native_password;
+SET character-set-server=utf8mb4;
+SET collation-server=utf8mb4_unicode_ci;
+
 insert into incidentes_historico(incidente_aberto_corretivo,incidente_sem_alocacao) values(1,1);
 insert into incidentes_historico(incidente_aberto_corretivo,incidente_sem_alocacao) values(2,2);
 insert into incidentes_historico(incidente_aberto_corretivo,incidente_sem_alocacao) values(3,3);
