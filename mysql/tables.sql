6c3af843b676b197981320c2b17de8a01dbf67ab
Rafal Leite <rafaelcostaleite@gmail.com>
2022-10-05 16:00:31 -0300

    ajustando docker mysql

--- mysql/tables.sql
+++ mysql/tables.sql
@@ -1,15 +1,7 @@
-CREATE TABLE IF NOT EXISTS incidentes_historico (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    data DATE,
-    incidente_aberto_corretivo INT,
-    incidente_aberto_corretivo_30 INT,	
-    incidente_sem_classificacao INT,
-    incidente_sem_alocacao INT);
-
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
-SET character-set-server=utf8mb4;
-SET collation-server=utf8mb4_unicode_ci;
-
-insert into incidentes_historico(incidente_aberto_corretivo,incidente_sem_alocacao) values(1,1);
-insert into incidentes_historico(incidente_aberto_corretivo,incidente_sem_alocacao) values(2,2);
-insert into incidentes_historico(incidente_aberto_corretivo,incidente_sem_alocacao) values(3,3);
+--CREATE TABLE IF NOT EXISTS incidentes_historico (
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--    data DATE,
+--    incidente_aberto_corretivo INT,
+--    incidente_aberto_corretivo_30 INT,	
+--    incidente_sem_classificacao INT,
+--    incidente_sem_alocacao INT);
