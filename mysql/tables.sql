2dc34628d10c27238acf34e880010942f808dd29
Rafal Leite <rafaelcostaleite@gmail.com>
2022-10-07 22:20:13 -0300

    Sucesso conexão com bandos de dados.

--- mysql/tables.sql
+++ mysql/tables.sql
@@ -2,6 +2,4 @@ CREATE TABLE IF NOT EXISTS incidentes_historico (
    id INT AUTO_INCREMENT PRIMARY KEY,
     data DATE,
     incidente_aberto_corretivo INT,
-    incidente_aberto_corretivo_30 INT,	
-    incidente_sem_classificacao INT,
-    incidente_sem_alocacao INT);
+    incidente_aberto_corretivo_30 INT);
